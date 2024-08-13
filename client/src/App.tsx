796a180b0860e70a16c2b412384b4f7035b387d8
Khanh <Khanh13Nguyen13@gmail.com>
2024-08-13 22:56:40 +0200

    add Router & Link

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,27 @@
-import { Route, Routes } from "react-router-dom";
+import { Outlet, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Navigation from "./components/Navigation";
-import MyFriends from "./components/MyFriends";
-import MyPortfolio from "./components/MyPortfolio";
-import JoinOurTeam from "./components/JoinOurTeam";
-import ContactUs from "./components/ContactUs";
+import MyFriends from "./components/pages/MyFriends";
+import MyPortfolio from "./components/pages/MyPortfolio";
+import JoinOurTeam from "./components/pages/JoinOurTeam";
+import ContactUs from "./components/pages/ContactUs";
+import NotFoundPage from "./components/pages/NotFoundPage";
 
 function App() {
 	return (
 		<div className="App">
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/my-friends" element={<MyFriends />} />
-				<Route path="/my-portfolio" element={<MyPortfolio />} />
-				<Route path="/join-our-team" element={<JoinOurTeam />} />
-				<Route path="/contact-us" element={<ContactUs />} />
-			</Routes>
+			<header>
+				<Navigation />
+				<Routes>
+					<Route path="/" element={<Home />}></Route>
+					<Route path="/my-friends" element={<MyFriends />} />
+					<Route path="/my-portfolio" element={<MyPortfolio />} />
+					<Route path="/join-our-team" element={<JoinOurTeam />} />
+					<Route path="/contact-us" element={<ContactUs />} />
+					<Route path="*" element={<NotFoundPage />} />
+				</Routes>
+			</header>
+			<Outlet />
 		</div>
 	);
 }
--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,18 +1,20 @@
-import { useEffect, useState } from "react";
-
-interface NavObj {
-	id: number;
-	linkInfo: string;
-}
-
-const navList = ["my friends", "contact us", "join our team", "my portfolio"];
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export default function Navigation() {
 	useEffect(() => {}, []);
 
 	return (
 		<nav className="text-2xl w-3/5 mx-auto">
-			<ul className="flex justify-center">Navigation</ul>
+			<ul className="flex justify-center">
+				<li>
+					<Link to={"/"}>Home</Link>
+					<Link to={"/my-friends"}>MyFriends</Link>
+					<Link to={"/my-portfolio"}>My Portfolio</Link>
+					<Link to={"/join-our-team"}>Join our team</Link>
+					<Link to={"/contact-us"}>Contact us</Link>
+				</li>
+			</ul>
 		</nav>
 	);
 }
