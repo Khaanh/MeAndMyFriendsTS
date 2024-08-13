19e7e3e4d2f07645d0088098824535792dda4a65
Khanh <Khanh13Nguyen13@gmail.com>
2024-08-13 23:19:12 +0200

    Add Outlet component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import "./styles/App.css";
 import { Outlet, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Navigation from "./components/Navigation";
@@ -7,18 +8,21 @@ import JoinOurTeam from "./components/pages/JoinOurTeam";
 import ContactUs from "./components/pages/ContactUs";
 import NotFoundPage from "./components/pages/NotFoundPage";
 
+//TODO: MyFriends page
+
 function App() {
 	return (
 		<div className="App">
 			<header>
-				<Navigation />
 				<Routes>
-					<Route path="/" element={<Home />}></Route>
-					<Route path="/my-friends" element={<MyFriends />} />
-					<Route path="/my-portfolio" element={<MyPortfolio />} />
-					<Route path="/join-our-team" element={<JoinOurTeam />} />
-					<Route path="/contact-us" element={<ContactUs />} />
-					<Route path="*" element={<NotFoundPage />} />
+					<Route path="/" element={<Navigation />}>
+						<Route index element={<Home />} />
+						<Route path="my-friends" element={<MyFriends />} />
+						<Route path="my-portfolio" element={<MyPortfolio />} />
+						<Route path="join-our-team" element={<JoinOurTeam />} />
+						<Route path="contact-us" element={<ContactUs />} />
+						<Route path="*" element={<NotFoundPage />} />
+					</Route>
 				</Routes>
 			</header>
 			<Outlet />
--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,20 +1,24 @@
 import { useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export default function Navigation() {
 	useEffect(() => {}, []);
 
 	return (
-		<nav className="text-2xl w-3/5 mx-auto">
-			<ul className="flex justify-center">
-				<li>
-					<Link to={"/"}>Home</Link>
-					<Link to={"/my-friends"}>MyFriends</Link>
-					<Link to={"/my-portfolio"}>My Portfolio</Link>
-					<Link to={"/join-our-team"}>Join our team</Link>
-					<Link to={"/contact-us"}>Contact us</Link>
-				</li>
-			</ul>
-		</nav>
+		<>
+			<nav className="text-2xl w-3/5 mx-auto mb-10">
+				<ul className="flex justify-center">
+					<Link to={""}>Home</Link>
+					<Link to={"my-friends"}>MyFriends</Link>
+					<Link to={"my-portfolio"}>My Portfolio</Link>
+					<Link to={"join-our-team"}>Join our team</Link>
+					<Link to={"contact-us"}>Contact us</Link>
+				</ul>
+			</nav>
+
+			<main className="container mx-auto">
+				<Outlet />
+			</main>
+		</>
 	);
 }
